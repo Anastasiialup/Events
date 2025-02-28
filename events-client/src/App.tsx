4d663728446464be839ac547d923e333214e1976
Anastasiialup <vt221_laa@student.ztu.edu.ua>
2025-02-28 21:37:38 +0200

    Cookie popup added

--- events-client/src/App.tsx
+++ events-client/src/App.tsx
@@ -1,13 +1,19 @@
 import { ApolloProvider } from '@apollo/client';
 import client from './services/graphql/client';
 import MainContent from './components/MainContent/MainContent';
+import CookiePopup from './components/CookiePopup/CookiePopup';
+
+const Home = () => {
+    return <CookiePopup />;
+};
 
 const App = () => {
-  return (
-    <ApolloProvider client={ client }>
-      <MainContent />
-    </ApolloProvider>
-  )
-}
+    return (
+        <ApolloProvider client={client}>
+            <Home />
+            <MainContent />
+        </ApolloProvider>
+    );
+};
 
-export default App
+export default App;
